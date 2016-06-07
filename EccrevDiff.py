d934c13dda855bbab4c942981c677240a8410347
Natanael Arndt <arndtn@gmail.com>
2016-06-07 15:37:44 +0200

    Clean up eccrev presentation

--- EccrevDiff.py
+++ EccrevDiff.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta
 from QuitDiffSerializer import QuitDiffSerializer
-from rdflib import URIRef, BNode, Literal, Namespace, Graph, ConjunctiveGraph
-from rdflib.namespace import RDF
+from rdflib import Namespace, ConjunctiveGraph
+from rdflib.namespace import RDF, NamespaceManager
 import uuid
 
 
@@ -9,31 +9,32 @@ class EccrevDiff(metaclass=ABCMeta):
     def serialize(self, add, delete):
 
 
-        diff = Namespace("urn:diff:" + str(uuid.uuid1()) + ":")
+        commit = Namespace("urn:commit:" + str(uuid.uuid1()) + ":")
         eccrev = Namespace("https://vocab.eccenca.com/revision/")
         #@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 
         g = ConjunctiveGraph()
-        g.add( (diff.diff, RDF.type, eccrev.Commit) )
+        namespace_manager = NamespaceManager(g)
+        namespace_manager.bind('eccrev', eccrev, override=False)
 
+        g.add( (commit.term(""), RDF.type, eccrev.Commit) )
 
         graphUris = set(delete.keys()) | set(add.keys())
 
         for graphUri in graphUris:
             if (graphUri in delete.keys() and len(delete[graphUri]) > 0) or (graphUri in add.keys() and len(add[graphUri]) > 0):
-                revId = "rev-" + str(uuid.uuid1())
-                graphTerm = diff.term(revId)
-                g.add( (diff.diff, eccrev.hasRevision, graphTerm) )
-                g.add( (graphTerm, RDF.type, eccrev.Revision) )
-                g.add( (graphTerm, eccrev.hasRevisionGraph, graphUri) )
+                revision = Namespace("urn:revision:" + str(uuid.uuid1()) + ":")
+                g.add( (commit.term(""), eccrev.hasRevision, revision.term("")) )
+                g.add( (revision.term(""), RDF.type, eccrev.Revision) )
+                g.add( (revision.term(""), eccrev.hasRevisionGraph, graphUri) )
                 if graphUri in delete.keys() and len(delete[graphUri]) > 0:
-                    deleteGraphName = diff.term(revId + ":delete")
-                    g.add( (graphTerm, eccrev.deltaDelete, deleteGraphName) )
+                    deleteGraphName = revision.term(":delete")
+                    g.add( (revision.term(""), eccrev.deltaDelete, deleteGraphName) )
                     for triple in delete[graphUri]:
                         g.add(triple + (deleteGraphName,))
                 if graphUri in add.keys() and len(add[graphUri]) > 0:
-                    insertGraphName = diff.term(revId + ":insert")
-                    g.add( (graphTerm, eccrev.deltaInsert, insertGraphName) )
+                    insertGraphName = revision.term(":insert")
+                    g.add( (revision.term(""), eccrev.deltaInsert, insertGraphName) )
                     for triple in add[graphUri]:
                         g.add(triple + (insertGraphName,))
 
