d6c15f8af15e3e1c764ed3ec600bc172e16631bc
Natanael Arndt <arndtn@gmail.com>
2016-06-07 14:36:40 +0200

    Use urn: s for temp uris

--- EccrevDiff.py
+++ EccrevDiff.py
@@ -9,7 +9,7 @@ class EccrevDiff(metaclass=ABCMeta):
     def serialize(self, add, delete):
 
 
-        diff = Namespace("http://example.org/" + str(uuid.uuid1()) + "/")
+        diff = Namespace("urn:diff:" + str(uuid.uuid1()) + ":")
         eccrev = Namespace("https://vocab.eccenca.com/revision/")
         #@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 
@@ -27,12 +27,12 @@ class EccrevDiff(metaclass=ABCMeta):
                 g.add( (graphTerm, RDF.type, eccrev.Revision) )
                 g.add( (graphTerm, eccrev.hasRevisionGraph, graphUri) )
                 if graphUri in delete.keys() and len(delete[graphUri]) > 0:
-                    deleteGraphName = diff.term(revId + "/delete")
+                    deleteGraphName = diff.term(revId + ":delete")
                     g.add( (graphTerm, eccrev.deltaDelete, deleteGraphName) )
                     for triple in delete[graphUri]:
                         g.add(triple + (deleteGraphName,))
                 if graphUri in add.keys() and len(add[graphUri]) > 0:
-                    insertGraphName = diff.term(revId + "/insert")
+                    insertGraphName = diff.term(revId + ":insert")
                     g.add( (graphTerm, eccrev.deltaInsert, insertGraphName) )
                     for triple in add[graphUri]:
                         g.add(triple + (insertGraphName,))
