34690f02b75130d8f702cd31d7bfdfcacdfe2658
Natanael Arndt <arndtn@gmail.com>
2024-08-08 09:34:33 +0200

    Sketch output of SPARQL graph patterns as turtle

--- quit_diff/serializer/SparqlDiff.py
+++ quit_diff/serializer/SparqlDiff.py
@@ -1,34 +1,39 @@
 from abc import ABCMeta
 from ..QuitDiff import QuitDiffSerializer
-
+from rdflib.plugins.sparql.processor import prepareUpdate
+from rdflib.plugins.sparql.algebra import pprintAlgebra
 
 class SparqlDiff(metaclass=ABCMeta):
     """Serialize RDF deltas using Sparql Insert/Delete Queries."""
+    def __init__(self):
+        self.preparedUpdate = prepareUpdate("delete data {}; insert data {}")
 
     def serialize(self, add, delete):
         deleteQuadPattern = ""
         insertQuadPattern = ""
         for graphUri, graph in delete.items():
             if len(graph) > 0:
+                # self.preparedUpdate.algebra[0].triples.extend(graph.triples((None,None,None)))
                 if str(graphUri) != "http://quitdiff.default/":
                     deleteQuadPattern += "\ngraph <%s> {\n%s\n}\n" % (
                         graphUri,
-                        graph.serialize(format="nt").strip(),
+                        graph.serialize(format="turtle").strip(),
                     )
                 else:
                     deleteQuadPattern += "\n%s\n" % (
-                        graph.serialize(format="nt").strip()
+                        graph.serialize(format="turtle").strip()
                     )
         for graphUri, graph in add.items():
             if len(graph) > 0:
+                # self.preparedUpdate.algebra[1].triples.extend(graph.triples((None,None,None)))
                 if str(graphUri) != "http://quitdiff.default/":
                     insertQuadPattern += "\ngraph <%s> {\n%s\n}\n" % (
                         graphUri,
-                        graph.serialize(format="nt").strip(),
+                        graph.serialize(format="turtle").strip(),
                     )
                 else:
                     insertQuadPattern += "\n%s\n" % (
-                        graph.serialize(format="nt").strip()
+                        graph.serialize(format="turtle").strip()
                     )
 
         query = ""
