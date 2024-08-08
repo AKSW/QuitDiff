8188512cc050963f7cfe5d593ee3e067fc5fdc3f
Natanael Arndt <arndtn@gmail.com>
2024-08-08 09:32:50 +0200

    Add sketch for a merge method

--- quit_diff/QuitDiff.py
+++ quit_diff/QuitDiff.py
@@ -177,3 +177,45 @@ class QuitDiff:
             "addB": addBGraph,
             "delB": delBGraph,
         }
+
+    def merge(self, local, remote, merged, base, diffFormat="sparql"):
+        # TODO
+        if local:
+            self.local = self.readIsomorphicGraph(local)
+
+        if remote:
+            self.remote = self.readIsomorphicGraph(remote)
+
+        if merged:
+            self.merged = self.readIsomorphicGraph(merged)
+
+        if base:
+            self.base = self.readIsomorphicGraph(base)
+
+        add = {}
+        remove = {}
+        # TODO
+
+        graphUris = set(self.local.keys()) | set(self.remote.keys())
+
+        for uri in graphUris:
+            if uri in self.local.keys() and uri in self.remote.keys():
+                localGraph = self.local[uri]
+                remoteGraph = self.remote[uri]
+                in_both, in_first, in_second = compare.graph_diff(
+                    localGraph, remoteGraph
+                )
+                add[uri] = in_second
+                remove[uri] = in_first
+            elif uri in self.local.keys():
+                remove[uri] = self.local[uri]
+            elif uri in self.remote.keys():
+                add[uri] = self.remote[uri]
+            else:
+                True
+
+        module = diffFormat.title() + "Diff"
+        diff = getattr(import_module("quit_diff.serializer." + module), module)
+
+        diffSerializer = diff()
+        print(diffSerializer.serialize(add, remove))
